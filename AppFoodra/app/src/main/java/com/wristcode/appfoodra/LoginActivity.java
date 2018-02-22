b83098b0793e631aedc9533d7125055f587937bc
AJAY\Ajay Jagadish <ajayjagadish19793@gmail.com>
2018-02-22 12:43:02 +0530

    Commits

--- AppFoodra/app/src/main/java/com/wristcode/appfoodra/LoginActivity.java
+++ AppFoodra/app/src/main/java/com/wristcode/appfoodra/LoginActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.wristcode.appfoodra.R;
@@ -20,6 +22,8 @@ public class LoginActivity extends AppCompatActivity
     TextView txtwelcome, txtsignin, emailtxt, passtxt, txtforgotpass, txtnewuser;
     EditText txtemail, txtpassword;
     Button loginButton;
+    ImageView hidepass;
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class LoginActivity extends AppCompatActivity
         txtemail = (EditText) findViewById(R.id.txtemail);
         passtxt = (TextView) findViewById(R.id.passtxt);
         txtpassword = (EditText) findViewById(R.id.txtpassword);
+        hidepass = (ImageView) findViewById(R.id.hidepass);
         txtforgotpass = (TextView) findViewById(R.id.txtforgotpass);
         txtnewuser = (TextView) findViewById(R.id.txtnewuser);
         loginButton = (Button) findViewById(R.id.loginButton);
@@ -45,7 +50,49 @@ public class LoginActivity extends AppCompatActivity
         txtforgotpass.setTypeface(font1);
         txtnewuser.setTypeface(font1);
         loginButton.setTypeface(font1);
+        txtpassword.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+        hidepass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(flag == 0)
+                {
+                    txtpassword.setTransformationMethod(null);
+                    hidepass.setImageResource(R.drawable.visible);
+                    flag = 1;
+                }
+                else if(flag == 1)
+                {
+                    txtpassword.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+                    hidepass.setImageResource(R.drawable.invisible);
+                    flag = 0;
+                }
+            }
+        });
     }
+
+    public class AsteriskPasswordTransformationMethod extends PasswordTransformationMethod {
+        @Override
+        public CharSequence getTransformation(CharSequence source, View view) {
+            return new PasswordCharSequence(source);
+        }
+
+        private class PasswordCharSequence implements CharSequence {
+            private CharSequence mSource;
+            public PasswordCharSequence(CharSequence source) {
+                mSource = source; // Store char sequence
+            }
+            public char charAt(int index) {
+                return '*'; // This is the important part
+            }
+            public int length() {
+                return mSource.length(); // Return default
+            }
+            public CharSequence subSequence(int start, int end) {
+                return mSource.subSequence(start, end); // Return default
+            }
+        }
+    };
 
     public void loginButton(View v)
     {
