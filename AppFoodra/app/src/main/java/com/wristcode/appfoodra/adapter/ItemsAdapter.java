a1aba97fb9c0d9cca47f2b203150b3501e06e615
AJAY\Ajay Jagadish <ajayjagadish19793@gmail.com>
2018-03-02 14:42:54 +0530

    Commits

--- AppFoodra/app/src/main/java/com/wristcode/appfoodra/adapter/ItemsAdapter.java
+++ AppFoodra/app/src/main/java/com/wristcode/appfoodra/adapter/ItemsAdapter.java
@@ -20,6 +20,8 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
