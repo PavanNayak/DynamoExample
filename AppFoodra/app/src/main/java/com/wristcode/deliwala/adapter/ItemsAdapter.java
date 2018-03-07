6fc34351b8da386650b9b4c56f4bf61f72eb76c2
AJAY\Ajay Jagadish <ajayjagadish19793@gmail.com>
2018-03-07 17:52:55 +0530

    Commits

--- AppFoodra/app/src/main/java/com/wristcode/deliwala/adapter/ItemsAdapter.java
+++ AppFoodra/app/src/main/java/com/wristcode/deliwala/adapter/ItemsAdapter.java
@@ -2,7 +2,10 @@ package com.wristcode.deliwala.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Typeface;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +18,7 @@ import com.bumptech.glide.Glide;
 import com.wristcode.deliwala.ItemActivity;
 import com.wristcode.deliwala.Pojo.Items;
 import com.wristcode.deliwala.R;
+import com.wristcode.deliwala.sqlite.ExampleDBHelper;
 
 import java.util.List;
 
@@ -22,13 +26,17 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.MyViewHolder
 {
     private List<Items> moviesList;
     private Context mContext;
+    ExampleDBHelper dbHelper;
+    SharedPreferences pref;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
         public TextView txtname, txtdesc, txtprice, txtminus, txtplus, txtadd, prodqty;
         RelativeLayout relative;
         ImageView image;
 
-        public MyViewHolder(View view) {
+        public MyViewHolder(View view)
+        {
             super(view);
             txtname = view.findViewById(R.id.txtname);
             txtdesc = view.findViewById(R.id.txtdesc);
@@ -39,6 +47,94 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.MyViewHolder
             prodqty = view.findViewById(R.id.prodqty);
             relative = view.findViewById(R.id.relative);
             image = view.findViewById(R.id.image);
+            dbHelper = new ExampleDBHelper(mContext);
+            pref = PreferenceManager.getDefaultSharedPreferences(mContext);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId())
+            {
+                case R.id.txtplus:
+                    int i = Integer.parseInt(prodqty.getText().toString());
+                    i++;
+                    prodqty.setText(String.valueOf(i));
+
+                    if (pref.getString("fg", "").toString().equals("0"))
+                    {
+                        dbHelper.insertItem(Integer.parseInt(sub_id.getText().toString()), sub_name.getText().toString(), Integer.parseInt(sub_qty.getText().toString()), Integer.parseInt(sub_price.getText().toString()), Integer.parseInt(img_type.getText().toString()), Integer.parseInt(sub_price.getText().toString()));
+                    } else if (pref.getString("fg", "").toString().equals("1")) {
+                        int value1 = Integer.parseInt(sub_id.getText().toString());
+                        Boolean value2 = dbHelper.checksubid(value1);
+                        if (value2.equals(true))
+                        {
+                            Cursor rs = dbHelper.getItem(Integer.parseInt(sub_id.getText().toString()));
+                            while (rs.moveToNext())
+                            {
+                                subname = rs.getString(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_NAME));
+                                subqty = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_QUANTITY));
+                                subprice = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_PRICE));
+                                imgtype = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_IMGTYPE));
+                                itemprice = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_ITEMPRICE));
+                                qty = subqty + 1;
+                                price = subprice + Integer.parseInt(sub_price.getText().toString());
+                                dbHelper.updateItem(value1, sub_name.getText().toString(), qty, price, imgtype, itemprice);
+                            }
+                        } else {
+                            dbHelper.insertItem(Integer.parseInt(sub_id.getText().toString()), sub_name.getText().toString(), Integer.parseInt(sub_qty.getText().toString()), Integer.parseInt(sub_price.getText().toString()), Integer.parseInt(img_type.getText().toString()), Integer.parseInt(sub_price.getText().toString()));
+                        }
+                    }
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("fg", "1");
+                    editor.apply();
+
+                    TOTAL = dbHelper.gettotalqty();
+                    RATE = dbHelper.gettotalprice();
+                    passprice(RATE);
+                    passval(TOTAL);
+                    break;
+
+
+                case R.id.txtminus:
+                    i = Integer.parseInt(prodqty.getText().toString());
+                    if (i > 0)
+                    {
+                        i--;
+                        prodqty.setText(String.valueOf(i));
+                        int value1 = Integer.parseInt(sub_id.getText().toString());
+                        Boolean value2 = dbHelper.checksubid(value1);
+                        if (value2.equals(true))
+                        {
+                            Cursor rs = dbHelper.getItem(Integer.parseInt(sub_id.getText().toString()));
+                            while (rs.moveToNext())
+                            {
+                                subname = rs.getString(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_NAME));
+                                subqty = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_QUANTITY));
+                                subprice = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_PRICE));
+                                imgtype = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_IMGTYPE));
+                                itemprice = rs.getInt(rs.getColumnIndex(ExampleDBHelper.SUBCAT_COLUMN_ITEMPRICE));
+                                qty = subqty - 1;
+                                price = subprice - Integer.parseInt(sub_price.getText().toString());
+                                if (qty == 0) {
+                                    dbHelper.deleteItem(Integer.parseInt(sub_id.getText().toString()));
+                                } else {
+                                    dbHelper.updateItem(value1, sub_name.getText().toString(), qty, price, imgtype, itemprice);
+                                }
+                            }
+                        } else {
+
+                        }
+
+                        TOTAL = dbHelper.gettotalqty();
+                        RATE = dbHelper.gettotalprice();
+                        passprice(RATE);
+                        passval1(TOTAL);
+                    } else if (i < 0 || i == 0) {
+                        prodqty.setText("0");
+                    }
+                    break;
+            }
+            dbHelper.close();
         }
     }
 
---
+++ AppFoodra/app/src/main/java/com/wristcode/deliwala/sqlite/ExampleDBHelper.java
@@ -0,0 +1,169 @@
+package com.wristcode.deliwala.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Ajay Jagadish on 07-Mar-18.
+ */
+
+public class ExampleDBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Deliwala.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final String SUBCAT_TABLE_NAME = "item";
+    public static final String SUBCAT_COLUMN_ID = "item_id";
+    public static final String SUBCAT_COLUMN_RESID = "res_id";
+    public static final String SUBCAT_COLUMN_NAME = "item_name";
+    public static final String SUBCAT_COLUMN_QUANTITY = "item_qty";
+    public static final String SUBCAT_COLUMN_PRICE = "item_price";
+    public static final String SUBCAT_COLUMN_IMAGE = "item_image";
+
+    public ExampleDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + SUBCAT_TABLE_NAME + "(" +
+                SUBCAT_COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                SUBCAT_COLUMN_RESID + " INTEGER, " +
+                SUBCAT_COLUMN_NAME + " TEXT, " +
+                SUBCAT_COLUMN_QUANTITY + " INTEGER, " +
+                SUBCAT_COLUMN_PRICE + " INTEGER, " +
+                SUBCAT_COLUMN_IMAGE + " TEXT)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + SUBCAT_TABLE_NAME);
+        onCreate(db);
+    }
+
+    public String insertItem(int item_id, int res_id, String item_name, int item_qty, int item_price, String item_image) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String Str_Return_Value = null;
+        long query_ret;
+        try {
+            ContentValues values = new ContentValues();
+            values.put(SUBCAT_COLUMN_ID, item_id);
+            values.put(SUBCAT_COLUMN_RESID, res_id);
+            values.put(SUBCAT_COLUMN_NAME, item_name);
+            values.put(SUBCAT_COLUMN_QUANTITY, item_qty);
+            values.put(SUBCAT_COLUMN_PRICE, item_price);
+            values.put(SUBCAT_COLUMN_IMAGE, item_image);
+            query_ret = db.insertWithOnConflict(SUBCAT_TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+            if (query_ret != -1) {
+                Str_Return_Value = "success";
+            } else {
+                Str_Return_Value = "failure";
+            }
+        } catch (Exception e) {
+        }
+        db.close();
+        return Str_Return_Value;
+    }
+
+    public boolean updateItem(int item_id, int res_id, String item_name, int item_qty, int item_price, String item_image) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(SUBCAT_COLUMN_ID, item_id);
+        contentValues.put(SUBCAT_COLUMN_RESID, res_id);
+        contentValues.put(SUBCAT_COLUMN_NAME, item_name);
+        contentValues.put(SUBCAT_COLUMN_QUANTITY, item_qty);
+        contentValues.put(SUBCAT_COLUMN_PRICE, item_price);
+        contentValues.put(SUBCAT_COLUMN_IMAGE, item_image);
+        db.update(SUBCAT_TABLE_NAME, contentValues, SUBCAT_COLUMN_ID + "=?", new String[]{Integer.toString(item_id)});
+        return true;
+    }
+
+    public Cursor getItem(int item_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + SUBCAT_TABLE_NAME + " WHERE " + SUBCAT_COLUMN_ID + "=" + item_id;
+        return db.rawQuery(selectQuery, null);
+    }
+
+    public Cursor getAllItems() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + SUBCAT_TABLE_NAME;
+        return db.rawQuery(selectQuery, null);
+    }
+
+    public Integer deleteItem(int item_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(SUBCAT_TABLE_NAME,
+                SUBCAT_COLUMN_ID + " = ? ",
+                new String[]{Integer.toString(item_id)});
+    }
+
+    public int gettotalqty() {
+        int qtyValue = 0;
+        SQLiteDatabase database = this.getWritableDatabase();
+        final Cursor cursor1 = database.rawQuery("SELECT SUM(sub_qty) FROM " + SUBCAT_TABLE_NAME, null);
+        if (cursor1 != null) {
+            try {
+                if (cursor1.moveToFirst()) {
+                    qtyValue = cursor1.getInt(0);
+                }
+            } finally {
+                cursor1.close();
+            }
+        }
+        return qtyValue;
+    }
+
+    public int gettotalprice() {
+        int priceValue = 0;
+        SQLiteDatabase database = this.getWritableDatabase();
+        final Cursor cursor1 = database.rawQuery("SELECT SUM(sub_price) FROM " + SUBCAT_TABLE_NAME, null);
+        if (cursor1 != null) {
+            try {
+                if (cursor1.moveToFirst()) {
+                    priceValue = cursor1.getInt(0);
+                }
+            } finally {
+                cursor1.close();
+            }
+        }
+        return priceValue;
+    }
+
+    public int getQuantity(int sub_id) {
+        int qty = 0;
+        SQLiteDatabase db = this.getReadableDatabase();
+        String strSql = "SELECT * FROM " + SUBCAT_TABLE_NAME + " WHERE " + SUBCAT_COLUMN_ID + " = " + sub_id;
+        Cursor cursor = db.rawQuery(strSql, null);
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            qty = cursor.getInt(cursor.getColumnIndex(SUBCAT_COLUMN_QUANTITY));
+        }
+        return qty;
+    }
+
+    public boolean checksubid(int item_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        final Cursor cursor = db.rawQuery("SELECT * FROM " + SUBCAT_TABLE_NAME + " WHERE " + SUBCAT_COLUMN_ID + "=" + item_id, null);
+        if (cursor.getCount() <= 0) {
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
+    public long getProfilesCount() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        long cnt = DatabaseUtils.queryNumEntries(db, SUBCAT_TABLE_NAME);
+        db.close();
+        return cnt;
+    }
+
+    public void deleteAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("Delete from " + SUBCAT_TABLE_NAME);
+    }
+}
